c8ef04f3285d8f54712e0e4c7856ded767471d2d
saqibJamil <saqib.jamil@appifytech.com>
2020-07-10 23:59:53 +0500

    working

--- woodling/src/components/search_talent.component.jsx
+++ woodling/src/components/search_talent.component.jsx
@@ -14,9 +14,17 @@ function SearchTalent(props) {
                 <div className="col-md-12 p0">
                     <div className="img-div h380 mb10 p-relative">
                         <img src={require('../assets/find-talent-cover.png')}  alt="Talent pic" />
-                    </div>
-                    <div className='w50'>
-                        <h1>Find Talents</h1>
+                        <div className='w50 p-absolute t0 ml45 mt100'>
+                            <h1 className=' clr-w fs50'><b>Find Talents</b></h1>
+                            <p className=' clr-w'><b>Explore our talent database<br/>Hunting down the perfect star has never been easier. Try it.</b></p>
+                            <div>
+                                <form class="example">
+                                    <i className='fa fa-search icon'></i>
+                                    <input type="search" placeholder="Who are you looking for?" name="search" className='border-radius' />
+                                    <button type="submit" name='searchSubmit' class="btn border-radius"><b>Search</b></button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
