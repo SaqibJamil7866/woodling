0d4242b2995997ee698bfe6e0c526019a5733b6d
saqibJamil <saqib.jamil@appifytech.com>
2020-07-12 11:23:14 +0500

    home screen

---
+++ woodling/src/services/ActivityStreamService.js
@@ -0,0 +1,48 @@
+  
+import axios from 'axios';
+import {AuthService} from './AuthService';
+import { activityStreamUrl, activityDetailsUrl, getPostTaggedUsersUrl } from '../public/endpoins';
+
+export const ActivityStreamService = {
+   getActivityStreams, getActivityDetails, getPostTaggedUsers     
+};
+
+function getActivityStreams(page) { 
+ return new Promise((resolve, reject) =>{
+ 		 
+ 		const url =  activityStreamUrl+"?page=+"+page+"&user_id="+AuthService.getUserId();
+        axios.get(url)
+		.then((res) => {
+		 	resolve(res);
+		})
+		.catch((error) => {
+            reject(error);
+		});
+	});
+}
+function getActivityDetails(postId) { 
+ return new Promise((resolve, reject) =>{		 
+		const url =  activityDetailsUrl+"?user_id="+AuthService.getUserId()+"&post_id="+postId;
+		axios.get(url)
+		.then((res) => {
+			resolve(res);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
+}
+function getPostTaggedUsers(postId) { 
+ return new Promise((resolve, reject) =>{
+		const url =  getPostTaggedUsersUrl+"?post_id="+postId;
+		axios.get(url)
+		.then((res) => {
+			resolve(res);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
+}
+
+ 
