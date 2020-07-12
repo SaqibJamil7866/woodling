b2aab6831c33557d764e711d8adcecbaea780cb7
saqibJamil <saqib.jamil@appifytech.com>
2020-07-12 13:23:33 +0500

    Revert "almost done"
    This reverts commit 4a9a04f42ed6378046356056988d0e4e9f6693ce.

--- woodling/src/components/search_talent.component.jsx
+++ woodling/src/components/search_talent.component.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState, useReducer } from 'react';
 import axios from 'axios';
 import TalentBar from './common/talent_bar.component';
-import TalentCard from './common/talent_card.component';
 
 function SearchTalent(props) {
-
+    
     useEffect(() => {
 
     }, []);
@@ -30,26 +29,15 @@ function SearchTalent(props) {
                 </div>
             </div>
 
-            <div className="row d-flex m0">
-                <div className="col-md-8 br-white pl100">
+            <div className="container flex">
+                <div className="col-md-8 br-white">
                     <TalentBar title="Featured Talents of the week"/>
-                    <div className='mt20'>
-                        {/* <div> */}
-                            <TalentCard />
-                            <TalentCard />
-                            <TalentCard />
-                            <TalentCard />
-                            <TalentCard />
-                            <TalentCard />
-                            <TalentCard />
-                        {/* </div> */}
-                    </div>
                 </div>
                 <div className="col-md-4">
                     <div className="img-div h230 mt30 mb10">
                         <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"  alt="authore pic"/>
                     </div>
-
+                    
                     <div className="mt10 mb10">
                         {/* <ExploreCard /> */}
                     </div>
