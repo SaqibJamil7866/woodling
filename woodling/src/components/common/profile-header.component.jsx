1d83d080275d43fee8647a1c2ee7bad3e1266baa
saqibJamil <saqib.jamil@appifytech.com>
2020-07-25 00:03:18 +0500

    Site url

--- woodling/src/components/common/profile-header.component.jsx
+++ woodling/src/components/common/profile-header.component.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import RatingStar from './rating-stars.component';
 import convertToFloat from '../../public/helperFunctions';
@@ -5,11 +6,11 @@ import { siteUrl } from '../../public/endpoins';
 
 const ProfilePicHeader = (props) => {
     const {rating, profile_picture, username} = props;
-    console.log(profile_picture)
+    console.log(siteUrl, profile_picture)
     return ( 
         <div className='d-flex space-between w50'>
             <RatingStar rating={convertToFloat(rating)} />
-            <img className='border-radius60 h100 m-50' src={siteUrl+""+profile_picture} />
+            <img className='border-radius60 h100 m-50' src={siteUrl+""+profile_picture} alt="profile pic" />
             <p><b>@{username}</b></p>
         </div>
      );
--- woodling/src/components/common/small_submission_card.component.jsx
+++ woodling/src/components/common/small_submission_card.component.jsx
@@ -8,7 +8,7 @@ const SmallSubmissionCard = (props) => {
         <div className='clr__white w45 d-flex flex-dir-col p15 border-radius-20 '>
             <div className='d-flex space-between align-items-center'>
                 <div>
-                    <h1 className='fs25 clr__red'>{data.length}</h1>
+                    <h1 className='fs25 clr__red'>{data && data.length}</h1>
                 </div>
                 <div>
                     <p onClick={()=>cardClick()} className="gray pointer"><b>View All </b><span className="inline-block float-right ml-2"> <RightIcon /></span></p>
@@ -16,7 +16,7 @@ const SmallSubmissionCard = (props) => {
             </div>
             <div>
                 <h3 className='fs20 align-right w100p'>{Heading}</h3>
-                {from==='jobs' ?<p className='w100p align-right fs17'>You have {data.length} casting calls posted</p> : <p className='w100p align-right fs17'>You have sent an application to {data.length} casting calls</p>}
+                {from==='jobs' ?<p className='w100p align-right fs17'>You have {data && data.length} casting calls posted</p> : <p className='w100p align-right fs17'>You have sent an application to {data.length} casting calls</p>}
             </div>
         </div>
     );
