cfcf424e2c77b10903bbc813c9acb2b9111f5a0a
saqibJamil <saqib.jamil@appifytech.com>
2020-08-08 06:36:02 +0500

    Side bar

--- woodling/src/components/common/sidebar.component.jsx
+++ woodling/src/components/common/sidebar.component.jsx
@@ -6,6 +6,11 @@ import { ReactComponent as TrendingIcon } from '../../assets/discovery-unselecte
 import { ReactComponent as CallsIcon } from '../../assets/casting-calls-icon.svg';
 import { ReactComponent as SearchIcon } from '../../assets/talent-search.svg';
 import { ReactComponent as MarketplaceIcon } from '../../assets/marketplace-naira.svg';
+import { ReactComponent as MailIcon } from '../../assets/mail.svg';
+import { ReactComponent as BellIcon } from '../../assets/bell.svg';
+import { ReactComponent as CogIcon } from '../../assets/cog.svg';
+import { ReactComponent as MainSearchIcon } from '../../assets/search.svg';
+import { ReactComponent as DotsIcon } from '../../assets/dots.svg';
 import { BrowserRouter as Router, Route, Link, withRouter } from "react-router-dom";
 
 const StyledSideNav = styled.div`   
@@ -25,37 +30,69 @@ class SideNav extends React.Component {
         super(props);
         this.state = {
             activePath: props.location.pathname,
-            items: [
+            topIcons: [
                 {
                   path: '/home', /* path is used as id to check which NavItem is active basically */
                   name: 'Home',
-                  imgsrc: (<HomeIcon alt="Home icon" title="Home"/>),
+                  imgsrc: (<HomeIcon alt="Home icon" title="Home" height="30px" />),
                   key: 1 /* Key is required, else console throws error. Does this please you Mr. Browser?! */
                 },
                 {
                   path: '/trending',
                   name: 'Trending',
-                  imgsrc: (<TrendingIcon alt="Trending icon" title="Trending" width="30px" />),
+                  imgsrc: (<TrendingIcon alt="Trending icon" title="Trending" height="30px" width="30px" />),
                   key: 2
                 },
                 {
                   path: '/casting_calls',
                   name: 'Casting Calls',
-                  imgsrc: (<CallsIcon alt="Calls icon" title="Calls" />),
+                  imgsrc: (<CallsIcon alt="Calls icon" title="Calls" height="35px" />),
                   key: 3
                 },
                 {
                   path: '/search_talent',
                   name: 'Talent Search',
-                  imgsrc: (<SearchIcon alt="Search icon" title="Search" />),
+                  imgsrc: (<SearchIcon alt="Search icon" title="Search" height="30px" />),
                   key: 4
                 },
                 {
                     path: '/market_place',
                     name: 'Market Place',
-                    imgsrc: (<MarketplaceIcon alt="Market icon" title="Marketplace" />),
+                    imgsrc: (<MarketplaceIcon alt="Market icon" title="Marketplace" height="30px" />),
                     key: 5
                 },
+            ],
+            bottomIcons: [
+                {
+                  path: '/chat', /* path is used as id to check which NavItem is active basically */
+                  name: 'Chat',
+                  imgsrc: (<MailIcon alt="Chat icon" title="Chat" height="30px" width="29px" />),
+                  key: 11 /* Key is required, else console throws error. Does this please you Mr. Browser?! */
+                },
+                {
+                  path: '/alerts',
+                  name: 'Alerts',
+                  imgsrc: (<BellIcon alt="Bell icon" title="Alerts" width="29px" height="30px" />),
+                  key: 12
+                },
+                {
+                  path: '/settings',
+                  name: 'Settings',
+                  imgsrc: (<CogIcon alt="Settings icon" title="Settings" height="30px" />),
+                  key: 13
+                },
+                {
+                  path: '/search',
+                  name: 'Search',
+                  imgsrc: (<MainSearchIcon alt="Search icon" title="Search" height="30px" />),
+                  key: 14
+                },
+                {
+                  path: '/mainprofile',
+                  name: 'Main Profile',
+                  imgsrc: (<DotsIcon alt="Dots icon" title="Profile" height="30px" />),
+                  key: 15
+                }
               ]
         }
     }
@@ -65,11 +102,26 @@ class SideNav extends React.Component {
     }
 
     render() {
-        const { items, activePath } = this.state;
+        const { topIcons, bottomIcons, activePath } = this.state;
         return(
             <StyledSideNav>
                 {
-                    items.map((item) => {
+                    topIcons.map((item) => {
+                        return (
+                            <NavItem
+                                path={item.path}
+                                name={item.name}
+                                imgsrc={item.imgsrc}
+                                onItemClick={this.onItemClick}
+                                active={item.path === activePath}
+                                key={item.key}
+                            />
+                        );
+                    })
+                }
+                <div><hr className="m0" /></div>
+                {
+                    bottomIcons.map((item) => {
                         return (
                             <NavItem 
                                 path={item.path}
@@ -90,8 +142,8 @@ class SideNav extends React.Component {
 const RouterSideNav = withRouter(SideNav);
 
 const StyledNavItem = styled.div`
-    height: 70px;
-    width: 60px; /* width must be same size as NavBar to center */
+    height: 50px;
+    width: 50px; /* width must be same size as NavBar to center */
     text-align: center; /* Aligns <a> inside of NavIcon div */
     margin-bottom: 0;   /* Puts space between NavItems */
     a {
