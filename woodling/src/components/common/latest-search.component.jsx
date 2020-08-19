98ac377f6c9ba43228e00e6d11087219a30bbf74
New_fyp <aqilmanzoor850@gmail.com>
2020-08-19 05:50:23 -0700

    arrow

--- woodling/src/components/common/latest-search.component.jsx
+++ woodling/src/components/common/latest-search.component.jsx
@@ -13,24 +13,19 @@ const LatestSearch = (props) => {
                 <div className='col-md-12'>
                     <div className='d-flex clr__white scrolling-x'>
                         {peoples.map((i, index) => {
-<<<<<<< HEAD
-                            return <div className='d-flex flex-dir-col align-items-center justify-content-center p10 mlr10'>  
-                                        <div className='d-flex justify-content-center w70'>
-                                            <img onError={onCrash} className='border-radius60 w30 h80' src={picUrl+""+i.profile_thumb}  alt='thumbnail pic' />
-=======
                             return (
                                 <div key={index} className='d-flex flex-dir-col align-items-center justify-content-center p5'>  
-                                        <div className='d-flex justify-content-center'>
-                                            <img onError={onCrash} className='border-radius60 w110 h110' src={picUrl+""+i.profile_thumb}  alt='thumbnail pic' />
->>>>>>> master
-                                        </div>
-                                        <p style={{width: '110px'}} className='mb0 fs12 alignCenter'>{i.full_name}</p>
-                                        <div style={{width: '110px'}}>
-                                            <RatingStar rating={convertToFloat(i.rating)} />
-                                        </div>
+                                    <div className='d-flex justify-content-center'>
+                                        <img onError={onCrash} className='border-radius60 w110 h110' src={picUrl+""+i.profile_thumb}  alt='thumbnail pic' />
+                                    </div>
+                                    <p style={{width: '110px'}} className='mb0 fs12 alignCenter'>{i.full_name}</p>
+                                    <div style={{width: '110px'}}>
+                                        <RatingStar rating={convertToFloat(i.rating)} />
+                                    </div>
                                 </div>
                             );
                         })}
+                        {peoples.length>8 ? <i className='fa fa-arrow-right right-arrow pointer' /> : null}
                     </div>
                 </div>
                 <div className='col-md-9'>
